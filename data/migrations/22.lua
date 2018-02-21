c385bc3f55ebc44fb00b7a22bf19bb5417a6fb3d
Leandro Matheus <kesuhige@gmail.com>
2018-02-21 19:33:13 -0300

    Add migration 23

--- data/migrations/22.lua
+++ data/migrations/22.lua
@@ -1,3 +1,6 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 22 (UTF-8 encoding)")
+	db.query("CREATE TABLE IF NOT EXISTS `pokemon` (`id` int(11) NOT NULL AUTO_INCREMENT, `type` varchar(255) NOT NULL,`nickname` varchar(255) NOT NULL,`gender` int(1) NOT NULL DEFAULT '0', `nature` int(2) NOT NULL DEFAULT '0', `conditions` blob NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;")	
+
+	return true
 end
