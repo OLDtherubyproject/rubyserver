9afdb7adbea957608c4dee0c7632db961625f347
Leandro Matheus <kesuhige@gmail.com>
2018-02-21 19:30:14 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -340,6 +340,16 @@ CREATE TABLE IF NOT EXISTS `towns` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
+CREATE TABLE IF NOT EXISTS `pokemon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `gender` int(1) NOT NULL DEFAULT '0',
+  `nature` int(2) NOT NULL DEFAULT '0',
+  `conditions` blob NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+
 DROP TRIGGER IF EXISTS `ondelete_players`;
 DROP TRIGGER IF EXISTS `oncreate_guilds`;
 
