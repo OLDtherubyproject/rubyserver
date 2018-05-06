709aa2e9fd7c054c6e1a263e6a4453e4e31fd2f0
Nycholas de Sousa <nycholas@live.com>
2018-05-06 00:04:00 -0300

    Improved Potions
    - Added Exhaust lib to improve potions
    - New messages on use potions
    - Improved some conditions

--- data/actions/scripts/other/potions.lua
+++ data/actions/scripts/other/potions.lua
@@ -1,33 +1,54 @@
+local exhaustion = 1000
+local seconds = 45
+
 local potions = {
-	[8877] = 200,
-	[8878] = 500,
-	[8879] = 2000,
+	[8877] = 200, -- Potion, restore 200hp
+	[8878] = 500, -- Super Potion, restore 500hp
+	[8879] = 2000, -- Hyper Potion, restore 2000hp
 }
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if type(target) == "userdata" and not target:isPokemon() then
-		return false
+		player:sendCancelMessage("You can only use potion in the pokemon.")
+		return true
 	end
-
+	
 	if target:getMaster() ~= player then
 		return false
 	end
-
+	
+	if player:getExhaustion(exhaustion) > 0 then
+		player:sendCancelMessage("You have to wait ".. player:getExhaustion(exhaustion) .." seconds to use this potion.")
+		return true
+    end
+	
 	local potion = potions[item:getId()]
-	if potion then
+	if potion and not (target:getHealth() == target:getMaxHealth())then
 		target:addHealth(potion)
-		item:remove(1)
-	end
-
-	if item:getId() == 8880 or item:getId() == 8881 then
-		if item:getId() == 8881 then
-			target:clearConditions();
-		end
-
-		target:addHealth(target:getMaxHealth())
+		target:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+		player:setExhaustion(exhaustion, seconds)
 		item:remove(1)
 		return true
 	else
-		return false
+		player:sendCancelMessage("This Pokemon does not need any potion.")
+		return true
 	end
+
+	if item:getId() == 8880 then
+		target:addHealth(target:getMaxHealth())
+		target:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
+		player:setExhaustion(exhaustion, seconds)
+		item:remove(1)
+		return true
+	end
+	
+	if item:getId() == 8881 then
+		target:cleanConditions();
+		target:getPosition():sendMagicEffect(CONST_ME_MAGIC_RED)
+		player:setExhaustion(exhaustion, seconds)
+		item:remove(1)
+		return true
+	end
+	
+	return false
 end
